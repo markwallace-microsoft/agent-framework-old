8e3c2ab2855a943f4e66f98d42bb13c02b969af5
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-06-03 14:39:50 +0100

    Move Agendefinition model classes which support

---
+++ dotnet/src/Microsoft.Agents.Abstractions/Definition/AgentCreationOptions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+namespace Microsoft.Agents;
+
+/// <summary>
+/// Optional parameters for agent creation used when create an <see cref="Agent"/>
+/// using an instance of <see cref="AgentFactory"/>.
+/// <remarks>
+/// Implementors of <see cref="AgentFactory"/> can extend this class to provide
+/// agent specific creation options.
+/// </remarks>
+/// </summary>
+public class AgentCreationOptions
+{
+}
---
+++ dotnet/tests/Microsoft.Agents.Abstractions.UnitTests/Definition/AgentDefinitionModelTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Microsoft.Agents;
+
+namespace SemanticKernel.Agents.UnitTests.Definition;
+
+/// <summary>
+/// Unit testing of <see cref="AgentDefinition"/> and related model classes.
+/// </summary>
+public class AgentDefinitionModelTests
+{
+    /// <summary>
+    /// Verify ModelDefinition.Api cannot be null or whitespace.
+    /// </summary>
+    [Fact]
+    public void VerifyModelDefinitionApiNotNullOrWhiteSpace()
+    {
+        // Arrange
+        var modelDefinition = new ModelDefinition();
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => modelDefinition.Api = "");
+        Assert.Throws<ArgumentNullException>(() => modelDefinition.Api = null!);
+    }
+}
